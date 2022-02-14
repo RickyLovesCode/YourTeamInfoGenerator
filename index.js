37d5b57e7ced823d9ed5b461b3bff337cc49a61c
Ricky27 <Rickyrenteria7@gmail.com>
2022-02-13 19:06:28 -0800

    final prompts added

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
 const path = require("path");
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const generatePage = require('./src/pageTemplate')
 
 
 
 
-//PROMPTS FOP EMPLOYEE INFO
-const promptUser = () => {
+//PROMPTS FOP MANAGER INFO
+const promptManager = () => {
     return inquirer.prompt([{
 
         type: 'input',
         name: 'name',
-        message: 'what is the employee name?',
+        message: "what is the manager's name?",
         validate: nameInput => {
             if (nameInput) {
                 return true;
@@ -22,12 +28,37 @@ const promptUser = () => {
         }
 
     },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'what is the manager id number?',
+        validate: id => {
+            if (id) {
+                return true;
+            } else {
+                console.log('please enter id number');
+                return false;
+            };
 
+        }
+
+
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'what is the manager email?'
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'please enter a office number'
+    },
     {
         type: 'list',
         name: 'role',
-        message: 'what is the employee role?',
-        choices: ["Engineer", "Intern", "Manager"],
+        message: 'would you like to add another employee to your team?',
+        choices: ["Engineer", "Intern"],
         validate: roleInput => {
             if (roleInput) {
                 return true;
@@ -36,17 +67,22 @@ const promptUser = () => {
                 return false;
             }
         }
-    },
+    }
 
-    {
+    ])
+}
+
+const promptEngineer = () => {
+    return inquirer.prompt([{
+
         type: 'input',
-        name: 'id',
-        message: 'what is the employee id number?',
-        validate: idInput => {
-            if (idInput) {
+        name: 'name',
+        message: "what is the Engineer's name?",
+        validate: nameInput => {
+            if (nameInput) {
                 return true;
             } else {
-                console.log('please enter id number');
+                console.log('please enter a name!');
                 return false;
             }
         }
@@ -54,12 +90,122 @@ const promptUser = () => {
     },
     {
         type: 'input',
-        name: 'email',
-        message: 'what is the employee email?'
+        name: 'id',
+        message: "what is the Engineer's ID number?",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log('please enter a id number!');
+                return false;
+            }
+        }
     },
 
-    ])
-}
+    {
+        type: 'input',
+        name: 'email',
+        message: "please enter the Engineer's email"
+    },
+    {
+        type: 'input',
+        name: "enter the engineer's github",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('please enter the github name')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'would you like to add another employee to your team?',
+        choices: ["Engineer", "Intern"],
+        validate: roleInput => {
+            if (roleInput) {
+                return true;
+            } else {
+                console.log('please choose a role')
+                return false;
+            }
+        }
+    }
 
-promptUser();
+    ])
+};
+
+const promptIntern = () => {
+    return inquirer.prompt([{
+
+        type: 'input',
+        name: 'name',
+        message: "what is the Intern's name?",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('please enter a name!');
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "what is the Intern's ID number?",
+        validate: idInput => {
+            if (idInput) {
+                return true;
+            } else {
+                console.log('please enter a id number!');
+                return false;
+            }
+        }
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: "please enter the Intern's email"
+    },
+    {
+        type: 'input',
+        name: "enter the Intern's school",
+        validate: schoolInput => {
+            if (schoolInput) {
+                return true;
+            } else {
+                console.log('please enter the school name')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'would you like to add another employee to your team?',
+        choices: ["Engineer", "Intern"],
+        validate: roleInput => {
+            if (roleInput) {
+                return true;
+            } else {
+                console.log('please choose a role')
+                return false;
+            }
+        }
+    }
+
+    ])
+};
+
+
+
+promptManager()
+    .then(promptEngineer)
+    .then(promptIntern);
+
 
